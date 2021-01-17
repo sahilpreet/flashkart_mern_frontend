4334da01f3ee2ed82164d6e48ffd76885fec097e
sahil991 <sahilpreetsingh9915545834@gmail.com>
2021-01-17 18:14:33 +0530

    Updates

---
+++ precache-manifest.162389684f3e520a293e7850e73053c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a305d7244b131062ae16bfabab10d94",
+    "url": "/flashkart_mern_frontend/index.html"
+  },
+  {
+    "revision": "b3e9ec9bc3be43e6d224",
+    "url": "/flashkart_mern_frontend/static/css/main.b5d5dfff.chunk.css"
+  },
+  {
+    "revision": "1d1079d112841f8f6faa",
+    "url": "/flashkart_mern_frontend/static/js/2.2aefaa05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flashkart_mern_frontend/static/js/2.2aefaa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e9ec9bc3be43e6d224",
+    "url": "/flashkart_mern_frontend/static/js/main.84f73cb1.chunk.js"
+  },
+  {
+    "revision": "df94213f9c810703a5b6",
+    "url": "/flashkart_mern_frontend/static/js/runtime-main.2fc1f98b.js"
+  }
+]);
